131459933e6071bf611c5fa972436306ec02796e
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-03-18 14:26:05 -0300

    Added auto elevating check

--- removeKebab.ps1
+++ removeKebab.ps1
@@ -1,4 +1,12 @@
 
+## Auto Elevating Script
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
+
 function Processo
 {
 	param($name)
